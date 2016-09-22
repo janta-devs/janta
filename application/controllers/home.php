4b9a834797bfcef6c0d24427e2d9055b30344ba1
Antony Ngayo <theantonymars@gmail.com>
2016-09-22 14:01:56 +0300

    messing around with the code

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -3,7 +3,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 class home extends CI_Controller{
-	public function index(){
+
+	public function index()
+	{
 		$this->load->view('home');
 	}
 }
