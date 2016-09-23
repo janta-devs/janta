a1dccfbb4c9f1b0a4a6ce4c062748bff08eb7445
Dan <ochiodhis@gmail.com>
2016-09-23 15:45:20 +0300

    Changed the layout and look of home.php

--- application/views/home.php
+++ application/views/home.php
@@ -2,15 +2,43 @@
 <html>
 <head>
 	<title>Home</title>
-	<link rel="stylesheet" type="text/css" href="<?php print base_url();?>assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="<?php print base_url();?>assets/bower_components/bootstrap/dist/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php print base_url();?>assets/bower_components/bootstrap/dist/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="<?php print base_url();?>assets/bower_components/bootstrap/dist/css/template.css">
+	<link rel="stylesheet" type="text/css" href="<?php print base_url();?>assets/bower_components/bootstrap/dist/css/jumbotron.css">
 </head>
 <body>
-
-<div class="container-fluid">
-	<header class="jumbotron">
-		<h1 style="font-style: cursive;">Janta</h1>
-	</header>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+<div class="container">
+	<div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+            <div class="logo-home" ></div>
+        </div>
+         <div id="navbar" class="navbar-collapse collapse">
+          <form class="navbar-form navbar-right">
+            <div class="form-group">
+              <input type="text" placeholder="Email" class="form-control">
+            </div>
+            <div class="form-group">
+              <input type="password" placeholder="Password" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-success">Sign in</button>
+          </form>
+        </div><!--/.navbar-collapse -->
+        </div>
+        </nav>
+<!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Looking for Work?</h1>
+        <p>Join the #1 global workplace to get access to millions of gigs and job opportunities from thousands of employers near you.</p>
+        <p><a class="btn btn-primary btn-lg" href="#" role="button">Sign Up now &raquo;</a></p>
+      </div>
+    </div>
 	<div id="start" class="col-md-12 pull-right"></div>
 </div>
 
