494f7ef10b0c9967bb722dfbb2eb13745246eb28
Antony Ngayo <theantonymars@gmail.com>
2016-09-26 09:16:49 +0300

    housekeeping in the login.js file

--- assets/js/login.js
+++ assets/js/login.js
@@ -28,7 +28,7 @@ signup_button.on('click', (function(event) {
 	.done(function( res ) {
 		console.log( res );
 		if( res['status'] == 'registered'){
-			alert_div.show('slow').removeClass('alert-danger').addClass('alert-success').html('Successfully registered');
+			// alert_div.show('slow').removeClass('alert-danger').addClass('alert-success').html('Successfully registered');
 			window.location.href = "http://localhost/janta/index.php/registration/step_two";
 		}else if( res['exists'] == true ){
 			alert_div.show('slow').removeClass('alert-success').addClass('alert-danger').html('User is already register');
