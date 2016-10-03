3ff3c6670af6ccf0de0e3ee0f46f22c98a858d77
Antony Ngayo <theantonymars@gmail.com>
2016-10-03 10:37:15 +0300

    housekeeping

--- application/controllers/Employee_registration.php
+++ application/controllers/Employee_registration.php
@@ -9,15 +9,16 @@ class Employee_registration extends CI_Controller{
 	{
 		$this->load->view('choice_page');
 	}
-	public function employeeData(){
+	public function employeeData()
+	{
 		$data = $this->input->post();
 		$data['login_id'] = 6;
 		$this->load->model('Employee');
 		$Employee = new Employee();
 		$Employee->insert( $data );
 	}
-
-	public function profile(){
+	public function profile()
+	{
 		$this->load->model('Employee');
 		$employee = new Employee();
 		$data['user_info'] = $employee->get();
