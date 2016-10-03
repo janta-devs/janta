52f34ec37696686d97402b82e4e344725012dc40
janta-devs <jadzantonyngayo@gmail.com>
2016-10-03 10:23:40 +0300

    some housekeeping

--- application/controllers/Employee_registration.php
+++ application/controllers/Employee_registration.php
@@ -11,7 +11,7 @@ class Employee_registration extends CI_Controller{
 	}
 	public function employeeData(){
 		$data = $this->input->post();
-		$data['login_id'] = 6;
+		$data['login_id'] = 15;
 		$this->load->model('Employee');
 		$Employee = new Employee();
 		$Employee->insert( $data );
