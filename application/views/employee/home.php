17cc32d0f9f432783b48811fde46433b1528fbdc
Dan <ochiodhis@gmail.com>
2016-10-12 15:08:30 +0300

    corrections in home.php

--- application/views/employee/home.php
+++ application/views/employee/home.php
@@ -4,7 +4,6 @@
 	<title>Home</title>
 	<link rel="stylesheet" type="text/css" href="<?php print base_url();?>assets/bower_components/bootstrap/dist/css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="<?php print base_url();?>assets/bower_components/bootstrap/dist/css/template.css">
-<<<<<<< HEAD
   <link rel="stylesheet" href="<?php print base_url();?>assets/bower_components/bootstrap/dist/css/intlTelInput.css">
 	<!-- <link rel="stylesheet" type="text/css" href="<?php print base_url();?>assets/bower_components/bootstrap/dist/css/jumbotron.css"> -->
   <!--<script type="text/javascript">
@@ -13,9 +12,6 @@
     $("#navbar input:first").focus();
   });
   </script>-->
- 
-=======
->>>>>>> 1cf5f05c2f0eb1260c699a0cb49edde718012431
 </head>
 <body>
 <nav class="navbar navbar-inverse navbar-fixed-top">
@@ -45,29 +41,15 @@
          ?>
            <div class="row">
             <div id="email" class="form-group">
-            <?php echo form_input(array(
-                'name' => 'email',
-                'id' => 'email',
-                'placeholder' => 'Email',
-                'class' => 'form-control',
-                'value' => set_value('email')
-            ));?>
-            <?php echo form_error('email') ?>
+              <input type="email" name="email" id="email" placeholder="Email" data-error="That email address is invalid" class="form-control" value="<?php set_value('email');?>" required>
             </div>
            <!-- -->
             <div class="form-group">
-            <?php echo form_password(array(
-                'name' => 'password',
-                'id' => 'password',
-                'placeholder' => 'Password',
-                'class' => 'form-control',
-                'value' => set_value('password')
-            ));?>
-            <?php echo form_error('password') ?>
+              <input type="password" name="password" id="password" placeholder="Password" class="form-control" data-minLength="6" value="<?php set_value('password');?>" required>
             </div>
             <?php echo form_submit(array(
                 'value' => 'Log In',
-                'class' => 'btn btn-success',
+                'class' => 'btn btn-primary btn-warning',
             )); ?>
             </div>
             <div class="row">
@@ -82,9 +64,9 @@
         </div>
         </nav>
 <!-- Main jumbotron for a primary marketing message or call to action -->
-  <div class="container-fluid">
-  <div class="row" id="form" style="margin-top: 90px;">
-  <div class="col-md-4 col-md-offset-1" >
+  <div class="container" style="padding: 0; ">
+  <div class="row" style="margin-top: 70px; margin-left: 0px; margin-right: 0px;">
+  <div class="col-md-4" >
     <h1>Sign Up!</h1>
         <p class='jumbotron-text'>Join the #1 global workplace.</p>
        <!-- <p><a class="btn btn-primary btn-lg" href="" role="button" id = "signup" data-toggle = "modal" data-target = "#register">Sign Up now &raquo;</a></p>-->   
@@ -92,32 +74,38 @@
     $fattr = array('class' => 'form-signin');
     echo form_open('/home/register/', $fattr); ?>
     <div class="form-group">
-      <?php echo form_input(array('name'=>'email_add', 'id'=> 'email_add', 'placeholder'=>'Your Email Address', 'class'=>'form-control', 'value' => set_value('email_add'))); ?>
-      <?php echo form_error('email_add');?>
+      <input type="email" for="email_add" name="email_add" class="form-control" placeholder="Your Email Address, e.g. example@example.com" data-error="Bruh, the email address is invalid" value="<?php set_value('email_add');?>" required>
+      <!--<?php /*echo form_input(array('name'=>'email_add', 'id'=> 'email_add', 'placeholder'=>'Your Email Address', 'class'=>'form-control', 'required', 'value' => set_value('email_add'))); ?>
+      <?php echo form_error('email_add');*/?>-->
+      <div class="help-block with-errors"></div>
     </div>
     <div class="form-group">
-      <?php echo form_input(array('name'=>'phone', 'id'=> 'phone', 'placeholder'=>'Mobile Phone', 'class'=>'form-control', 'value' => set_value('phone'))); ?>
-      <?php echo form_error('phone');?>
+      <input type="text" name="phone" id="phone" placeholder="Mobile Number" class="form-control" value="<?php set_value('phone');?>" required>
+      <div class="help-block with-errors"></div>
+      <!--<?php /*echo form_input(array('name'=>'phone', 'id'=> 'phone', 'placeholder'=>'Mobile Phone', 'class'=>'form-control', 'value' => set_value('phone'))); ?>
+      <?php echo form_error('phone');*/?>*/-->
     </div>
     <div class="form-group">
-      <?php echo form_input(array('name'=>'industry', 'id'=> 'industry', 'placeholder'=>'Industry or Sector', 'class'=>'form-control', 'value'=> set_value('industry'))); ?>
-      <?php echo form_error('industry');?>
+      <input type="text" name="industry" id="industry" placeholder="Industry or Sector, e.g. IT" class="form-control" value="<?php set_value('industry');?>" required>
+      <div class="help-block with-errors"></div>
     </div>
     <div class="form-group">
-      <?php echo form_input(array('name'=>'specialization', 'id'=> 'specialization', 'placeholder'=>'Specilization or Profession', 'class'=>'form-control', 'value'=> set_value('specialization'))); ?>
-      <?php echo form_error('specialization');?>
+      <input type="text" name="specialization" id="specialization" placeholder="Specialization or Profession" class="form-control" value="<?php set_value('specialization');?>" required>
+      <div class="help-block with-errors"></div>
     </div>
+    <div class="form-group">
     <?php echo form_submit(array('value'=>'Sign up', 'class'=>'btn btn-lg btn-primary btn-block')); ?>
+    </div>
     <?php echo form_close(); ?>
     </div>
-    <div class="col-md-4 col-md-offset-1">
+    <div class="col-md-4 col-md-offset-1" style="overflow: none;">
+      <div class="form-group">
       <img src="<?php print base_url();?>assets/bower_components/bootstrap/dist/images/sl1.jpg">
+      </div>
     </div>
   </div>
         
     </div>
-     
-<<<<<<< HEAD
  <!--Creating the modal for registration-->
  <!--<div class="modal" id = "register">
  	<div class="modal-dialog">
@@ -150,13 +138,28 @@
 <script>
   $("#phone").intlTelInput();
 </script>
-<!--<script type="text/javascript" src = "<?php print base_url();?>assets/bower_components/jquery/dist/jquery.min.js"></script>
-=======
+<script type="text/javascript">
+/*Sign up form validation*/
+  $(document).ready(function(){
+    $('.form-signin').bootstrapValidator({
+      message: 'This value is not valid',
+      feedbackIcons: {
+        valid: 'glyphicon glyphicon-ok',
+        invalid: 'glyphicon glyphicon-remove',
+        validating: 'glyphicon glyphicon-refresh'
+      },
+      fields: {
+        email: {
 
-</div>
+        }
+      }
+    })
+  });
+</script>
+<!--<script type="text/javascript" src = "<?php print base_url();?>assets/bower_components/jquery/dist/jquery.min.js"></script>-->
 
 </body>
->>>>>>> 1cf5f05c2f0eb1260c699a0cb49edde718012431
+
 
 </html>
 
